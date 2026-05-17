676eb0d44ec4fe94fdad3db35155fbdc1b11ac0a
Taneesha Kalra <49729371+taneeshaa@users.noreply.github.com>
2026-05-17 14:18:56 +0530

    Add annotations for reactivemongorepositories

--- FraudEngine/src/main/java/tanz/inc/fraudengine/FraudengineApplication.java
+++ FraudEngine/src/main/java/tanz/inc/fraudengine/FraudengineApplication.java
@@ -2,8 +2,10 @@ package tanz.inc.fraudengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class FraudengineApplication {
 
 	public static void main(String[] args) {
--- FraudEngine/src/main/java/tanz/inc/fraudengine/repository/FraudEvidenceRepository.java
+++ FraudEngine/src/main/java/tanz/inc/fraudengine/repository/FraudEvidenceRepository.java
@@ -1,7 +1,9 @@
 package tanz.inc.fraudengine.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import tanz.inc.fraudengine.entity.FraudEvidence;
 
+@Repository
 public interface FraudEvidenceRepository extends ReactiveMongoRepository<FraudEvidence, String> {
 }
